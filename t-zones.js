344503f82cee758f0bab78474256462b3c85deab
Petr <petr@kle.cz>
2013-08-22 13:31:24 +0200

    Kontrola načtení jquery, přihlášení do t-zones a odeslání zprávy.

--- t-zones.js
+++ t-zones.js
@@ -4,24 +4,39 @@ if(require('system').env["USERPROFILE"]){
 	home = 'USERPROFILE';
 }
 
-phantom.injectJs(require('system').env[home]+'/.jquery.js');
+var settingsfile = require('system').env[home]+'/.t-zones.json';
+var jqueryfile = require('system').env[home]+'/.jquery.js';
+
+phantom.injectJs(jqueryfile);
 
 var casper = require('casper').create({
 	verbose: true,
-	logLevel: "info",
-	loadImages: false,
-	loadPlugins: false
+	logLevel: 'info',
+  pageSettings: { loadImages: false, loadPlugins: false	}
 });
 
-casper.userAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.63 Safari/537.31');
+casper.start('https://www.t-mobile.cz');
 
-casper.start('https://www.t-mobile.cz', function() {
+casper.then(function() {
 		this.mouseEvent('mouseover', '.nav-last');
 });
 
 casper.then(function() {
-	var conf = jQuery.parseJSON(require('fs').read(require('system').env[home]+'/.t-zones.json'));
-  this.fill('form[action="https://www.t-mobile.cz/.gang/login-url/portal"]', { username: conf.login, password: conf.password }, true);
+	if(typeof jQuery === 'undefined'){
+     this.echo('jQuery not loaded! Check ' + jqueryfile, 'ERROR');
+		 this.exit();
+	}
+	var conf = jQuery.parseJSON(require('fs').read(settingsfile));
+  this.fill('form[action="https://www.t-mobile.cz/.gang/login-url/portal"]',
+		{ username: conf.login, password: conf.password },
+	 	 true);
+});
+
+casper.then(function() {
+	if (!this.exists('.ico-person-small-green-filled')) {
+     this.echo('Login to t-zones failed! Check ' + settingsfile, 'ERROR');
+		 this.exit();
+  }
 });
 
 casper.then(function() {
@@ -29,7 +44,18 @@ casper.then(function() {
 });
 
 casper.then(function() {
-    this.fill('form[action="#"]', { recipients: casper.cli.get("tel"), text: casper.cli.get("msg")}, true);
+    this.fill('form[action="#"]',
+			{ recipients: casper.cli.get("tel"), text: casper.cli.get("msg")},
+			 true);
+});
+
+casper.then(function() {
+	if (this.exists('.text-green')) {
+		this.echo(this.fetchText('.text-green'));
+	}else{
+     this.echo('Sending message failed!', 'ERROR');
+		 exit();
+	}
 });
 
 casper.run();
